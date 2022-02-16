ef99c1df0433b5789460b1b9a7fc579baee1e663
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-02-16 14:24:56 +0600

    add js

--- invoice.js
+++ invoice.js
@@ -12,3 +12,62 @@ document.getElementById('submit-buyer').addEventListener("click", function () {
     buyerDatails.value = ''
 
 })
+
+document.getElementById('add').addEventListener("click", function () {
+    const itemTable = document.getElementById('list-item');
+    const itemName = document.getElementById('Item-name');
+    const itemPrice = document.getElementById('price');
+    const itemQuantity = document.getElementById('quantity');
+
+    const totalPrice = parseInt(itemPrice.value) * parseInt(itemQuantity.value)
+
+    const createTR = document.createElement('tr');
+    const createTh = document.createElement('th');
+    const createTd1 = document.createElement('td');
+    const createTd2 = document.createElement('td');
+    const createTd3 = document.createElement('td');
+
+    createTd3.classList.add("item-total");
+    createTh.innerText = itemName.value;
+    createTd1.innerText = itemPrice.value;
+    createTd2.innerText = itemQuantity.value;
+    createTd3.innerText = totalPrice;
+
+    createTR.appendChild(createTh)
+    createTR.appendChild(createTd1)
+    createTR.appendChild(createTd2)
+    createTR.appendChild(createTd3)
+    itemTable.appendChild(createTR)
+
+    itemName.value = '';
+    itemPrice.value = '';
+    itemQuantity.value = '';
+
+    const subTotalInput = document.getElementById('sub-total');
+    subTotalInput.innerText = AddsubTotal();
+
+    const subTotalFinal = parseInt(subTotalInput.innerText);
+
+    const tax = AddsubTotal() * .15;
+    document.getElementById('tax').innerText = tax;
+
+    const totalAmount = subTotalFinal + tax;
+    document.getElementById('total-amount').innerText = totalAmount;
+    document.getElementById('total').innerText = totalAmount;
+
+
+})
+
+function AddsubTotal() {
+    let subTotal = 0;
+    const cost = document.getElementsByClassName('item-total');
+    for (let i = 0; i < cost.length; i++) {
+        const element = cost[i];
+        const price = parseInt(element.innerText)
+        subTotal = subTotal + price;
+
+    }
+
+    return subTotal;
+
+}
